1c785dd14c2c20965cc68af578b19d915dd569d3
GoldBeardSea <timbusch1@gmail.com>
2018-08-23 13:54:20 -0700

    pre pre lecture commit

---
+++ app/src/main/java/com/gbbeard/chatstatus/UserModels.java
@@ -0,0 +1,13 @@
+package com.gbbeard.chatstatus;
+
+public class UserModels {
+    public String username;
+    public String status;
+    public String statusText;
+
+    public UserModels(String username, String status, String statusText) {
+        this.username = username;
+        this.status = status;
+        this.statusText = statusText;
+    }
+}
