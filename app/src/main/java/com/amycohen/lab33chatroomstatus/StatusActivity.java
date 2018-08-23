80041da16da6e7dd677ede986233e2062de19d06
Amy Cohen <anbcohen@gmail.com>
2018-08-23 11:31:47 -0700

    started to add firebase to the project

--- app/src/main/java/com/amycohen/lab33chatroomstatus/StatusActivity.java
+++ app/src/main/java/com/amycohen/lab33chatroomstatus/StatusActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,18 +20,48 @@ import butterknife.ButterKnife;
 
 public class StatusActivity extends AppCompatActivity {
 
+    public static final String TAG = "FIREBASE: ";
     @BindView(R.id.list)
     RecyclerView list;
     private LinearLayoutManager linearLayoutManager;
     private StatusAdapter statusAdapter;
 
     List<Status> allStatuses;
+
+    FirebaseDatabase mDatabase;
+    DatabaseReference myRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
 
         ButterKnife.bind(this);
+
+
+        // Write a message to the database
+        mDatabase = FirebaseDatabase.getInstance();
+        myRef = mDatabase.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
         allStatuses = new ArrayList<>();
         allStatuses.add(new Status("amy", "online", "should totally be working on homework"));
@@ -35,6 +72,6 @@ public class StatusActivity extends AppCompatActivity {
 
         list.setLayoutManager(linearLayoutManager);
         list.setAdapter(statusAdapter);
-        
+
     }
 }
